c9a66e487032bd96b915e11c9dc2f025bfa1a64a
Divya552 <52657771+Divya552@users.noreply.github.com>
2019-07-10 18:03:35 +0530

    Create Assignment3.py

---
+++ Assignment3.py
@@ -0,0 +1,10 @@
+s='''hi ma'm good evening
+how r u
+bye bye'''
+print("length of string:", len(s))
+print("no of characters",len(s))
+b=s.split('\n')
+print(" no of lines: ",len(b))
+a=s.split()
+print("no of words:",len(a))
+
